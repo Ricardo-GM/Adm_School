156a80533cac5fc1405555ab6cf266f5c7398f25
RicardoGM20 <1611841@utp.edu.pe>
2020-09-08 18:58:45 -0500

    Añadido SplashScreen y redirección al Login

--- app/src/main/java/com/example/adm_school/MainActivity.java
+++ app/src/main/java/com/example/adm_school/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.adm_school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                getSupportActionBar().hide();
+
+                new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                        }
+                }, 4000);
+        }
 }
