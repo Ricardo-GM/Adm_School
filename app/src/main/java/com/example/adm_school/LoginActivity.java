961303213e6f20c10752123658eae703c7aacf3f
Luis Fernando Guerra <luisjeremias33.3@gmail.com>
2020-09-13 18:17:27 -0500

    Añadiendo modulo de Registro version 1

--- app/src/main/java/com/example/adm_school/LoginActivity.java
+++ app/src/main/java/com/example/adm_school/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.adm_school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView mTextViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mTextViewRegister = findViewById(R.id.textViewRegister);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/adm_school/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.adm_school;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
