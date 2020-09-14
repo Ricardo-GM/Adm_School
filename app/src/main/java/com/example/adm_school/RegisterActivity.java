61b7fb2f8f736bc8819a33ba527926f7c9b88d20
Luis Fernando Guerra <luisjeremias33.3@gmail.com>
2020-09-14 03:32:50 -0500

    Añadiendo modulo de Registro version 3

--- app/src/main/java/com/example/adm_school/RegisterActivity.java
+++ app/src/main/java/com/example/adm_school/RegisterActivity.java
@@ -3,13 +3,21 @@ package com.example.adm_school;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Spinner spineer1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        spineer1 = (Spinner) findViewById(R.id.spinner);
+
+        String [] opciones = {"CORDINADOR", "DIRECTOR", "PROFESOR", "ALUMNO"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,opciones);
+        spineer1.setAdapter(adapter);
     }
 }
