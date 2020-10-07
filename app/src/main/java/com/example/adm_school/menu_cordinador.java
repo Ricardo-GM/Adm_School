58534313fe3d3bfd405b857fd0a1ac913ab24779
Luis Fernando Guerra <luisjeremias33.3@gmail.com>
2020-10-07 02:39:41 -0500

    Diseño Menu cordinador - gestion usuarios cursos salones y clases

---
+++ app/src/main/java/com/example/adm_school/menu_cordinador.java
@@ -0,0 +1,14 @@
+package com.example.adm_school;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_cordinador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_cordinador);
+    }
+}
